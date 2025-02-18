1e239b99d40580c2fccf259386c186d75c409e1a
CletsyMedia <cletussam12@yahoo.com>
2025-02-18 17:51:49 +0100

    🚑️ Hotfix:  📅 18-02-2025 ⏳ 17:51:47

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   optimizeDeps: {
     include: ["moment"],
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
   },
 });
